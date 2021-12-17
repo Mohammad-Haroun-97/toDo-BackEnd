24dcdd4310eb2cc88b5b56e7c47ce98c024a0712
Mohammad Haroun <harounmohammad@yahoo.com>
2021-12-17 20:19:16 +0200

    finish

--- src/auth/models/tasks/model.js
+++ src/auth/models/tasks/model.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const clothesModel = (sequelize, DataTypes) => sequelize.define('Clothes', {
-  name: { type: DataTypes.STRING, required: true },
-  task: { type: DataTypes.STRING, required: true },
   complete: { type: DataTypes.BOOLEAN, required: true },
-  difficulty: { type: DataTypes.INTEGER, required: true }
+  difficulty: { type: DataTypes.INTEGER },
+  id:{ type: DataTypes.STRING, primaryKey: true},
+  name: { type: DataTypes.STRING, required: true },
+  task: { type: DataTypes.STRING, required: true }
+
 });
 
 module.exports = clothesModel;
--- src/auth/tasks.routes.js
+++ src/auth/tasks.routes.js
@@ -30,9 +30,17 @@ async function handleGetAll(req, res) {
 }
 
 async function handleGetOne(req, res) {
-  const id = req.params.id;
+  
+  try {
+    const id = req.params.id;
   let theRecord = await req.model.get(id)
   res.status(200).json(theRecord);
+
+  } catch (error) {
+    res.send(error.messege) 
+    
+  }
+  
 }
 
 async function handleCreate(req, res) {
